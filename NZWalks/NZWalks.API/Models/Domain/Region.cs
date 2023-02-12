198d471f158452db878cfdd7d9c435f1628e77d2
nivric <nived501@gmail.com>
2023-02-12 15:45:10 +0530

    Completed section 1

---
+++ NZWalks/NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public double Area { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+        public long Population { get; set; }
+
+        //Navigation Properties
+
+        public IEnumerable<Walk> Walks { get; set; }
+    }
+}
--- NZWalks/NZWalks.API/Program.cs
+++ NZWalks/NZWalks.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//inject DbContext
+builder.Services.AddDbContext<NZWalksDbContext>(options => { options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalks")); });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
